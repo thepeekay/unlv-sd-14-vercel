04a03739f550af9d6a485e592d44dcbf76b4e2ce
thepeekay <panagiotis@kefalidis.me>
2024-04-04 21:36:50 -0400

    Simple test app for vercel

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
+
+module.exports = app
